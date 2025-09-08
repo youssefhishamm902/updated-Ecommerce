5f766cf1c805e40514505fa23d1261f8c751c235
youssefhishamm902 <hishamy550@gmail.com>
2025-09-08 09:27:01 +0300

    dynamic routing fix

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'details/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
